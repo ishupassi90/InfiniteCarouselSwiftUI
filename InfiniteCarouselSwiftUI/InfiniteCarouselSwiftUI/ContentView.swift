5e9fc03f4d70125e39ac526a3f5a6c41e008b730
Ishu Passi <ishu@vaultspay.ae>
2024-08-28 14:10:22 +0530

    Infinte ScrollView

---
+++ InfiniteCarouselSwiftUI/InfiniteCarouselSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InfiniteCarouselSwiftUI
+//
+//  Created by Ishu Passi on 28/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InfiniteCarouselSwiftUI/InfiniteCarouselSwiftUI/InfiniteCarouselSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteCarouselSwiftUIApp.swift
+//  InfiniteCarouselSwiftUI
+//
+//  Created by Ishu Passi on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteCarouselSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
